f4748a4c473606e7d2b3de16cf355f0e811b9f16
Belili Fahem <fahem.belili.pro@gmail.com>
2023-06-09 12:35:29 +0200

    chrore(api-client): add configuration for open api generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Civility, GeothermikApiService, SupportEstimationDto, SupportEstimationRequestDto } from 'generated-sources/geothermik-api-client';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,39 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'geothermik-front-angular';
+  apiResponse?: SupportEstimationDto;
+
+  constructor(private geothermikApiService: GeothermikApiService) { }
+
+  ngOnInit(): void {
+    console.log('AppComponent ngOnInit');
+  }
+
+  sendSupportEstimationRequest(): void {
+    const supportEstimationRequest: SupportEstimationRequestDto = {
+      civility: Civility.M,
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@mail.com',
+      phoneNumber: '0123456789',
+      isOwner: true,
+      householdPersonCount: 2,
+      householdIncomes: {
+        value: 10000,
+      },
+      surface: 100,
+    };
+
+    this.geothermikApiService.estimateSupport(supportEstimationRequest).subscribe({
+      next: (response) => {
+        console.log('AppComponent > sendSupportEstimationRequest > response', response);
+        this.apiResponse = response;
+      },
+      error: (error) => {
+        console.log('AppComponent > sendSupportEstimationRequest > error', error);
+      }
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ApiModule, Configuration, ConfigurationParameters } from 'generated-sources/geothermik-api-client';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(ApiModule.forRoot(apiConfigFactory))
+  ]
 };
+
+export function apiConfigFactory(): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: 'http://localhost:3000'
+  }
+  return new Configuration(params);
+}
