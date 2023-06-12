ac7b9c7aa8e48d01507161dff3e25d442da842ef
Belili Fahem <fahem.belili.pro@gmail.com>
2023-06-12 17:04:26 +0200

    chore(linter, fomatter): add commit linter and hooks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideHttpClient()],
     })
   );
 
